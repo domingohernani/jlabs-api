ecb2fa69827437ff8a966db2bd67cac7c2abe522
domingohernani <dev.hernani.domingo@gmail.com>
2025-12-19 00:09:03 +0800

    feat: add jwt middleware

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/authRoute.js"
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        credentials: true
+    })
+);
+
+app.use("/api/", authRoutes)
+
+// simple route
+app.get("/", (req, res) => {
+    res.send("Hello!");
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
---
+++ src/middlewares/verifyJwt.js
@@ -0,0 +1,26 @@
+import * as jose from "jose";
+
+export const verifyJwt = async (req, res, next) => {
+    const accessToken = req.cookies?.accessToken;
+
+    console.log(accessToken);
+
+
+    try {
+        if (accessToken) {
+            const publicPEM = process.env.PUBLIC_KEY;
+            const publicKey = await jose.importSPKI(publicPEM, "RS256");
+            const { payload } = await jose.jwtVerify(accessToken, publicKey);
+            req.user = payload;
+            return next();
+        } else {
+            return res.status(401).json({ code: "ACCESS_TOKEN_EXPIRED" });
+        }
+    } catch (error) {
+        if (error instanceof jose.errors.JWTExpired) {
+            return res.status(401).json({ code: "ACCESS_TOKEN_EXPIRED" });
+        }
+        console.error("JWT verification error:", error);
+        return res.status(403).json({ message: "Invalid access token" });
+    }
+};
